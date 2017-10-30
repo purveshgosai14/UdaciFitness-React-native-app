3839141a5b28eadf6253c45e355b132a80b2b9e1
Purvesh Gosai <puri.ui369@gmail.com>
2017-10-30 16:24:16 -0500

    feat: update for date & time

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,8 +1,18 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import { getMetricMetaInfo } from '../utils/helpers'
+import React, { Component } from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
+import DateHeader from './DateHeader'
+
+function SubmitBtn ({ onPress }) {
+    return (
+        <TouchableOpacity
+            onPress={onPress}>
+            <Text>SUBMIT</Text>
+        </TouchableOpacity>
+    )
+}
 
 export default class AddEntry extends Component {
     state = {
@@ -11,60 +21,77 @@ export default class AddEntry extends Component {
         swim: 0,
         sleep: 0,
         eat: 0,
-    };
-
+    }
     increment = (metric) => {
-        const {max, step} = getMetricMetaInfo(metric);
+        const { max, step } = getMetricMetaInfo(metric)
 
         this.setState((state) => {
-            const count = state[metric] + step;
+            const count = state[metric] + step
 
             return {
                 ...state,
                 [metric]: count > max ? max : count,
             }
         })
-    };
-
+    }
     decrement = (metric) => {
-
         this.setState((state) => {
-            const count = state[metric] - getMetricMetaInfo(metric).step;
+            const count = state[metric] - getMetricMetaInfo(metric).step
 
             return {
                 ...state,
                 [metric]: count < 0 ? 0 : count,
             }
         })
-    };
-
+    }
     slide = (metric, value) => {
         this.setState(() => ({
-            [metric]: value,
+            [metric]: value
         }))
-    };
+    }
+    submit = () => {
+        const key = timeToString()
+        const entry = this.state
 
+        // Update Redux
+
+        this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
+
+        // Navigate to home
+
+        // Save to "DB"
+
+        // Clear local notification
+    }
     render() {
-        const metaInfo = getMetricMetaInfo();
+        const metaInfo = getMetricMetaInfo()
+
         return (
             <View>
+                <DateHeader date={(new Date()).toLocaleDateString()}/>
                 {Object.keys(metaInfo).map((key) => {
-                    const {getIcon, type, ...rest} = metaInfo[key];
-                    const value = this.state[key];
+                    const { getIcon, type, ...rest } = metaInfo[key]
+                    const value = this.state[key]
 
                     return (
                         <View key={key}>
                             {getIcon()}
                             {type === 'slider'
-                                ? <UdaciSlider value={value} onChange={(value) => this.slide(key, value)} {...rest}
+                                ? <UdaciSlider
+                                    value={value}
+                                    onChange={(value) => this.slide(key, value)}
+                                    {...rest}
                                 />
-                            : <UdaciSteppers value={value} onIncrement={() => this.increment(key)}
-                                             onDecrement={() => this.decrement(key)}
-                            {...rest}
-                            />}
+                                : <UdaciSteppers
+                                    value={value}
+                                    onIncrement={() => this.increment(key)}
+                                    onDecrement={() => this.decrement(key)}
+                                    {...rest}
+                                />}
                         </View>
                     )
                 })}
+                <SubmitBtn onPress={this.submit} />
             </View>
         )
     }
